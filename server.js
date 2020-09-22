375d47143230733503773f296d1a968594cb600d
dobromiryor <dobromir.yor@gmail.com>
2020-09-22 21:25:33 +0300

    added compression library for gzipping on heroku

--- server.js
+++ server.js
@@ -2,29 +2,31 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const path = require('path')
+const compression = require('compression')
 
-if(process.env.NODE_ENV !== 'production') require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const app = express();
 const port = process.env.PORT || 5000
 
+app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(cors())
 
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build' ,'index.html'))
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
   })
 }
 
 app.listen(port, error => {
-  if(error) throw error
+  if (error) throw error
   console.log('Server running on port: ' + port)
 })
 
@@ -36,7 +38,7 @@ app.post('/payment', (req, res) => {
   }
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
-    if(stripeErr){
+    if (stripeErr) {
       res.status(500).send({ error: stripeErr })
     } else {
       res.status(200).send({ success: stripeRes })
