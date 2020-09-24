e8fd18b4ac4bae443779d062e7df446bdd4c88df
dobromiryor <dobromir.yor@gmail.com>
2020-09-24 11:46:37 +0300

    added service worker, updated manifest

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 
+import { store, persistor } from './redux/store'
+import * as serviceWorker from './serviceWorker'
+
 import './index.css';
 import App from './App';
 
-import { store, persistor } from './redux/store'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -19,3 +21,8 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
